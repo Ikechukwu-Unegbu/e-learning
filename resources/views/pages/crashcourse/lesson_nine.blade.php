e03a84ec73ad0ac9fe0b40f695246d0871581b75
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2022-03-24 12:32:55 +0100

    m

--- resources/views/pages/crashcourse/lesson_nine.blade.php
+++ resources/views/pages/crashcourse/lesson_nine.blade.php
@@ -13,7 +13,11 @@
   <div class="container course-holder">
     <div class="course_left">
       <h2>Introduction to Blockchain Technology and Crypto Trading</h2>
-      <img style="" src="https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
+      <video width="320" height="240" controls>
+        <source src="{{asset('image/videos/9. Spot Trading.mp4')}}" type="video/mp4">
+        <!-- <source src="movie.ogg" type="video/ogg"> -->
+        Your browser does not support the video tag.
+      </video>
       <div class="details">
         <div class="title">
           <span id="playing">Playing Now:</span> <span id="current_play">Lorem ipsum dolor sit amet consectetur.</span>
